b82cc8bec418745c608603fb93a4e3bb89dc3e4d
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-08 09:53:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Digite o 1° valor: ");
+            a = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.Write("Digite o 2° valor: ");
+                b = int.Parse(Console.ReadLine());
+            }
+            while (a >= b);
+        }
+    }
+}
